b117fb29bbe1e1d8b4bc2b215d0e395246ca20ae
MerwanS29 <s.merwan@outlook.com>
2020-01-16 16:37:42 +0100

    modif userController + userRoute

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,6 +7,7 @@ const User = mongoose.model('User');
 exports.register = function(req, res) {
     var newUser = new User(req.body);
     newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
+    console.log('testhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
     newUser.save((error, user) => {
       if (error) {
         res.status(500);
@@ -21,7 +22,7 @@ exports.register = function(req, res) {
 exports.sign_in = function(req, res) {
     User.findOne({
         email: req.body.email
-      }, function(err, user) {
+      }, function(error, user) {
         if (error) {
             res.status(500);
             res.json({message: "Erreur serveur lors de la connexion"})
@@ -29,7 +30,7 @@ exports.sign_in = function(req, res) {
         if (!user) {
          res.status(401).json({ message: 'Mot de passe ou email erroné' });
        } else if (user) {
-         if (!user.comparePassword(req.body.password)) {
+         if (!user.comparePassword(req.body.hash_password)) {
            res.status(401).json({ message: 'Mot de passe ou email erroné' });
           } else {
             return res.json({token: jwt.sign({ email: user.email, nom: user.nom, prenom: user.prenom, _id: user._id}, 'nodejs_api')});
@@ -38,10 +39,26 @@ exports.sign_in = function(req, res) {
       });
 }
 
+
 exports.loginRequired = function(req, res, next) {
     if (req.user) {
         next();
       } else {
         return res.status(401).json({ message: 'Utilisateur non autorisé' });
       }
+}
+
+
+exports.get_all_users = (req, res) => {
+  User.find({}, (error, users) => {
+    if(error){
+      res.status(500);
+      console.log(error);
+      res.json({message: "Erreur serveur."});
+    }
+    else {
+      res.status(200);
+      res.json(users);
+    }
+  })
 }
