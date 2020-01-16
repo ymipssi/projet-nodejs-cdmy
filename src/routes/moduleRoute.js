71dc315ea98b817b3a51c0f151add1599f666fc7
csoumou223 <bintacheickna@gmail.com>
2020-01-16 05:47:15 +0100

    le commit du fichier moduleRoute.js

---
+++ src/routes/moduleRoute.js
@@ -0,0 +1,14 @@
+module.exports = (app) => {
+  const moduleController = require('../controllers/moduleController');
+  const jwtMiddleware = require('../middleware/jwtMiddleware');
+
+  app.route('/user/:user_id/modules') // req.params.
+  .get(moduleController.get_all_modules)
+  .post(jwtMiddleware.verify_token, moduleController.create_a_module);
+
+ app.route('/modules/id_module')
+  .all(jwtMiddleware.verify_token)
+  .get(moduleController.get_a_module)
+  .put(moduleController.update_a_module)
+  .delete(moduleController.delete_a_module);
+}
